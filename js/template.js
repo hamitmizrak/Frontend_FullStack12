0f901f1a2e4b0f06157112c4961bf38b90556ac5
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-15 16:18:36 +0300

    js tuto

--- js/template.js
+++ js/template.js
@@ -42,5 +42,57 @@ vocabulary="Js Öğreniyorum";
 var vocabulary;
 console.log(vocabulary);
 
-//////////////////////////////////////////////////////////////////////$personName23
-// OPERATORs
+//////////////////////////////////////////////////////////////////////
+// OPERATORS
+// + - * / %
+let number1=13,number2=2;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+// &&=VE ||=VEYA
+// =    ATAMA
+// ==   EŞİT AMA TÜRÜNE BAKMADAN
+// ===  EŞİT VE TÜRÜNEDE BAK
+// !=   EŞİT DEĞİL
+// x++
+// ++x
+// x--
+// --x
+
+//////////////////////////////////////////////////////////////////////
+// Kullanıcıdan veri almak
+// let userData=prompt("Lütfen adınızı ve soyadınız giriniz");
+// console.log(userData);
+
+// let num1=Number(prompt("Lütfen 1.sayıyı giriniz")) ;
+// let num2= parseInt(prompt("Lütfen 2.sayıyı giriniz"));
+// console.log(num1+num2);
+
+
+//////////////////////////////////////////////////////////////////////
+// undefined
+let user1;
+console.log(user1);
+
+// NaN
+console.log(4/"asd");
+
+// Infinity
+console.log(4/0);
+
+
+// Escape Character
+console.log("\"");
+console.log("1.satır");
+console.log("2.satır");
+console.log("1.satır\n2.satır");
+
+// new 
+// this
+// null
+
+///////////////////////////////////////////////////////////////////////
+// SORULAR
