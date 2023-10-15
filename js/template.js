7b63ec47df3a123ea4999b0cff855fb1ff321641
Hamit Mizrak <hamitmizrak@gmail.com>
2023-10-15 15:59:40 +0300

    Variable

---
+++ js/template.js
@@ -0,0 +1,46 @@
+ // <!-- HTML comment -->
+ // /* css comment */
+ // JS single comment
+//  /* multiple comment */
+
+//  window.console.log("console log");
+//  window.console.info("console info");
+//  window.console.warn("console warn");
+//  window.console.error("console error");
+// console.error("console error");
+
+// //window.alert("deneme")
+
+// window.document.write("Html sayfasına yazıldı <br/>")
+// document.write("Html sayfasına yazıldı")
+
+// " çift tırnak"
+// ' tek tırnak '
+// ` backtick `
+////////////////////////////////////////////////////////////
+// üğşçö
+var personName="Merhabalar";
+var personName=44;
+console.log(personName);
+
+let personName23="Elazığ";
+personName23="Harput";
+console.log(personName23);
+
+const $personName23="Gazi Cad , Abdullah paşa";
+console.log($personName23);
+
+let cityName="İstanbul";
+console.log( typeof cityName);
+cityName=44.23;
+console.log( typeof cityName);
+cityName=true;
+console.log( typeof cityName);
+
+// Hoisting
+vocabulary="Js Öğreniyorum";
+var vocabulary;
+console.log(vocabulary);
+
+//////////////////////////////////////////////////////////////////////$personName23
+// OPERATORs
